840afbbe9c8a3d6dbec354ade2a3a622f7123aa6
G <53386620+Gnola@users.noreply.github.com>
2020-01-03 23:29:24 -0800

    added all playlists

--- client/src/components/AllPlaylists.js
+++ client/src/components/AllPlaylists.js
@@ -125,55 +125,55 @@ class AllPlaylists extends Component {
       <div className='all-playlists'>
         <div className='playlist-rows' id='timeofday'>
           <h3>Time of Day</h3>
-          <div className='playlists' onClick={this.showMorning}> <h3>Morning</h3>
+          <div className='playlists' id='morning' onClick={this.showMorning}> <h3>Morning</h3>
             {(this.state.morning) ? <Morning morningSongs={this.props.morningSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showAfternoon}> <h3>Afternoon</h3>
+          <div className='playlists' id='afternoon' onClick={this.showAfternoon}> <h3>Afternoon</h3>
             {(this.state.afternoon) ? <Afternoon afternoonSongs={this.props.afternoonSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showEvening}> <h3>Evening</h3>
+          <div className='playlists' id='evening' onClick={this.showEvening}> <h3>Evening</h3>
             {(this.state.evening) ? <Evening eveningSongs={this.props.eveningSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showNighttime}> <h3>Nighttime</h3>
+          <div className='playlists' id='night' onClick={this.showNighttime}> <h3>Nighttime</h3>
             {(this.state.nighttime) ? <Nighttime nighttimeSongs={this.props.nighttimeSongs} /> : null}
           </div>
         </div>
         <div className='playlist-rows' id='seasons'>
           <h3>Seasons</h3>
-          <div className='playlists' onClick={this.showSpring}> <h3>Spring</h3>
+          <div className='playlists' id='spring' onClick={this.showSpring}> <h3>Spring</h3>
             {(this.state.spring) ? <Spring springSongs={this.props.springSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showSummer}> <h3>Summer</h3>
+          <div className='playlists' id='summer' onClick={this.showSummer}> <h3>Summer</h3>
             {(this.state.summer) ? <Summer summerSongs={this.props.summerSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showFall}> <h3>Fall</h3>
+          <div className='playlists' id='fall' onClick={this.showFall}> <h3>Fall</h3>
             {(this.state.fall) ? <Fall fallSongs={this.props.fallSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showWinter}> <h3>Winter</h3>
+          <div className='playlists' id='winter' onClick={this.showWinter}> <h3>Winter</h3>
             {(this.state.winter) ? <Winter winterSongs={this.props.winterSongs} /> : null}
           </div>
         </div>
         <div className='playlist-rows' id='weather'>
           <h3>Weather</h3>
-          <div className='playlists' onClick={this.showSunshine}> <h3>Sunshine</h3>
+          <div className='playlists' id='sunshine' onClick={this.showSunshine}> <h3>Sunshine</h3>
             {(this.state.sunshine) ? <Sunshine sunshineSongs={this.props.sunshineSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showRain}> <h3>Rain</h3>
+          <div className='playlists' id='rain' onClick={this.showRain}> <h3>Rain</h3>
             {(this.state.rain) ? <Rain rainSongs={this.props.rainSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showClouds}> <h3>Clouds</h3>
+          <div className='playlists' id='clouds' onClick={this.showClouds}> <h3>Clouds</h3>
             {(this.state.clouds) ? <Clouds cloudSongs={this.props.cloudSongs} /> : null}
           </div>
         </div>
         <div className='playlist-rows' id='location'>
           <h3>Locations</h3>
-          <div className='playlists' onClick={this.showWoods}> <h3>Woods</h3>
+          <div className='playlists' id='woods' onClick={this.showWoods}> <h3>Woods</h3>
             {(this.state.woods) ? <Woods woodSongs={this.props.woodSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showBeach}> <h3>Beach</h3>
+          <div className='playlists' id='beach' onClick={this.showBeach}> <h3>Beach</h3>
             {(this.state.beach) ? <Beach beachSongs={this.props.beachSongs} /> : null}
           </div>
-          <div className='playlists' onClick={this.showFields}> <h3>Fields</h3>
+          <div className='playlists' id='fields' onClick={this.showFields}> <h3>Fields</h3>
             {(this.state.fields) ? <Fields fieldSongs={this.props.fieldSongs} /> : null}
           </div>
         </div>
